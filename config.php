be3c8f7a2b81b26499d1657f76fcf445b826e6e4
rifkiarjundi <82265810+rifkiarjundi@users.noreply.github.com>
2023-01-20 02:32:57 +0700

    Add files via upload

---
+++ config.php
@@ -0,0 +1,7 @@
+<!-- Reprogramed By By Rifki Arjundi -->
+<?php
+	$conn = new mysqli("localhost","root","","cart_system");
+	if($conn->connect_error){
+		die("Connection Failed!".$conn->connect_error);
+	}
+?>
